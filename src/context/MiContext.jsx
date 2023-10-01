e72c907a9bd6169bd3add5fca9c8317a62dff44d
jorge2101719 <rusosky01@gmail.com>
2023-09-30 23:47:29 -0300

    corrección en un comentario

--- src/context/MiContext.jsx
+++ src/context/MiContext.jsx
@@ -7,7 +7,6 @@ export const MiContexto = createContext(); // Creación de un contexto (punto 1)
 // Se crea un hook con useState que se asigna como valor
 // del Provider creado (punto 3 del desafío)
 const ImageProvider = ({children}) => {
-  // Estado creado con useState que se pasará al valor de Provider (punto 3)
   const [fotos, setFotos] = useState(imagenes.photos);
 
   return(
